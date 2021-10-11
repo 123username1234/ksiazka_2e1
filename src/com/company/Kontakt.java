db93d2230ff8df8aabaf3e14acd0e2a51beed8d4
krzys938 <krzys938@gmial.com>
2021-10-11 17:23:59 +0200

    Dodanie odczytu pliku

---
+++ src/com/company/Kontakt.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Kontakt {
+
+    public String nazwa;
+    public int numer;
+
+    public Kontakt(String nazwa, int numer){
+        this.nazwa = nazwa;
+        this.numer = numer;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
+    static ArrayList<Kontakt> ksiazkaTelefoniczna;
+
     public static void main(String[] args) {
-	// write your code here
+
+        ksiazkaTelefoniczna = new ArrayList<Kontakt>();
+
+    }
+
+    public static void OdczytajZPliku() throws FileNotFoundException {
+
+        File plik = new File("Kontakty.txt");
+        Scanner sc = new Scanner(plik);
+
+        ksiazkaTelefoniczna.clear();
+
+        while (sc.hasNext()){
+            ksiazkaTelefoniczna.add(new Kontakt(sc.next(), sc.nextInt()));
+        }
     }
 }
